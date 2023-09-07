298b5a97d7111de4b74d2e6d46f31bbb3a5abeb7
soongu <naong526@naver.com>
2023-09-07 14:40:41 +0900

    # 필드와 지역변수, 스트링비교

--- src/day02/DancerMain.java
+++ src/day02/DancerMain.java
@@ -32,5 +32,9 @@ public class DancerMain {
         hong.stretch();
         hong.dance();
         hong.introduce();
+
+
+        System.out.println(kim);
+        System.out.println(park);
     }
 }
